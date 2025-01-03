da16990f8d9f0ca2047abdde26c15c834e0b8291
keerthi-chenchu <keerthichenchu546@gmail.com>
2025-01-03 10:42:35 +0530

    while loop

---
+++ Demo6.java
@@ -0,0 +1,18 @@
+public class Demo6 {
+    public static void main(String[] args) {
+        String username = "keerthi001";
+        String pwd = "keerthi_001";
+        while((username == "keerthi001") && (pwd == "keerthi_001")) {
+            System.out.println("Happy new year");
+            break;
+        }
+    }
+}
+
+
+
+
+
+
+
+
