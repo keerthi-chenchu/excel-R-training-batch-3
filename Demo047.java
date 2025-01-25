3a76738c651d0d7af264c248ad2d11507696dfb2
keerthi-chenchu <keerthichenchu546@gmail.com>
2025-01-25 12:23:59 +0530

    To count number of digits of a number

---
+++ Demo047.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Demo047 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        int count = 0;
+        if (number == 0) {
+            count = 1;
+        } else {
+            while (number != 0) {
+                number /= 10;
+                count++;
+            }
+        }
+
+        System.out.println("Number of digits: " + count);
+
+        scanner.close();
+    }
+}
