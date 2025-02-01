4ab993163f70ca99f549b283f3450285cbe7dd52
chenchu-keerthi <keerthichenchu546@gmail.com>
2025-02-01 12:35:48 +0530

    greatest among 3 numbers

---
+++ PDemo003.java
@@ -0,0 +1,16 @@
+class PDemo003 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c =30;
+        if (a > b){
+        System.out.println("b is greater");
+        }
+        else if (b > c){
+            System.out.println("b is greater");
+        }
+            else {
+                System.out.println("c is greater");
+            }
+        }
+}
