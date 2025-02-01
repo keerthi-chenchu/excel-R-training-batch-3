775c40d0a0d5d64858d67054a2409d3253305d26
chenchu-keerthi <keerthichenchu546@gmail.com>
2025-02-01 12:58:47 +0530

    login credintals

---
+++ PDemo005.java
@@ -0,0 +1,12 @@
+class PDemo005 {
+    public static void main(String[] args) {
+        String name = "keerthi";
+        String password = "hloo";
+        if ((name == "keerthi") && (password == "hloo")){
+            System.out.println("hello welcome");
+        }
+        else {
+            System.out.println("wrong credientials");
+        }
+    }
+}
