a84fbc48b7fb2e111c3e9be0924555c25cf2899a
keerthi-chenchu <keerthichenchu546@gmail.com>
2025-02-06 12:27:57 +0530

    Update Demo067.java

--- Demo067.java
+++ Demo067.java
@@ -1,11 +1,6 @@
-class Demo067{
-    public static void main(String[] args){
-        int i = 1;
-        int j = 20;
-        for(i=1;i<j;i++){
-        if(i % 2 == 0){
-        System.out.println(i);
-    }
-}
+class Demo067 {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6};
+        System.out.println("arr[1]: " + arr[1] + ", arr[3]: " + arr[3] + ", arr[5]: " + arr[5]);
     }
 }
